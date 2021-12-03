46c607ee8d57381140d817f293693029bfba9fe6
Igor Ermakov <77785633+GarriGarrison@users.noreply.github.com>
2021-12-03 08:43:08 +0300

    Версия 1.0
    Простой клиент

---
+++ Client.pas
@@ -0,0 +1,76 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, BlckSock, Windows, Messages, Variants;
+
+
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    btnConnect: TButton;
+    dtnSend: TButton;
+    edAddress: TEdit;
+    edPort: TEdit;
+    edRequestString: TEdit;
+    lbResponseStr: TEdit;
+    timerSend: TTimer;
+    procedure btnConnectClick(Sender: TObject);
+    procedure dtnSendClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    Client: TTCPBlockSocket;
+  public
+    { public declarations }
+  end;
+
+resourcestring
+  rsConnected = 'Подключено';
+
+const
+  cReadTimeout = 10000;
+
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.btnConnectClick(Sender: TObject);
+begin
+  Client:= TTCPBlockSocket.Create;//создаем объект
+  Client.RaiseExcept:=True;//показываем все исключения Winsock
+  Client.Connect(edAddress.Text,edPort.Text);//пробуем соединиться с сервером
+  ShowMessage(rsConnected);
+end;
+
+procedure TMainForm.dtnSendClick(Sender: TObject);
+begin
+  Client.SendString(edRequestString.Text);//отправляем строку на сервер
+  lbResponseStr.Caption:= Client.RecvPacket(cReadTimeout);//пробуем получить ответ
+  timerSend.Enabled:= true;
+end;
+
+procedure TMainForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  Client.Free;
+end;
+
+procedure TMainForm.TimerSendTimer(Sender: TObject);
+begin
+  dtnSend.Click;
+end;
+
+end.
